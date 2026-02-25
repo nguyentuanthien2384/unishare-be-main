b47d31a40705fd3ec6d90d4566daf9bd7b8d84f1
thiennt <thiennt2004@gmail.com>
2026-02-25 15:18:02 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    exposedHeaders: ['Content-Disposition'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 8000);
+}
+bootstrap();
