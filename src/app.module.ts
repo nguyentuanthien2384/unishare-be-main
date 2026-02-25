a8383a459f0b38c2861a49122233a23b52cb0053
thiennt <thiennt2004@gmail.com>
2026-02-25 15:31:34 +0700

    setup module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
+import { AdminModule } from './admin/admin.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { LogsModule } from './logs/logs.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    DocumentsModule,
+    AdminModule,
+    StatisticsModule,
+    LogsModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
